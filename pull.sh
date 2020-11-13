073bea1c452b8015cdcb8646b2a697ca4acf4230
presleyswilliam <presleyswilliam@gmail.com>
2020-11-13 14:17:25 -0500

    autoCommit

--- pull.sh
+++ pull.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-cd /var/www/html/WilseysSeniorDesignBois
+
+cd /var/www/html/warped2-models/models/pandemic/visualize
 sudo git pull
 
-#cd /home/pi/myscripts
+#cd /var/www/html/WilseysSeniorDesignBois
 #sudo git pull
+
+cd /home/pi/myscripts
+sudo git add .
+sudo git commit -m "autoCommit"
+sudo git push
+sudo git pull
